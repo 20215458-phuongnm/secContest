8c05bb98b294ceac0b3d236d9d3de139d7edfe8f
phuong.nguyenminh1 <minhphuonghatinh2003@gmail.com>
2025-10-14 18:45:24 +0700

    be

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Base URL configuration from environment variables
-const BASE_URL = 'http://localhost:3000' 
+const BASE_URL = 'http://seccom-be-production.up.railway.app' 
 
 // Create axios instance
 const axiosClient = axios.create({
